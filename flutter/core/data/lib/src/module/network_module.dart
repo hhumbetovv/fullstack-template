307350ae236239f157619807142525c4abadd524
Humbet Humbetov <hhumbetov@azercell.com>
2025-11-02 20:38:07 +0400

    [flutter] auth interceptor added

--- flutter/core/data/lib/src/module/network_module.dart
+++ flutter/core/data/lib/src/module/network_module.dart
@@ -1,5 +1,6 @@
 import 'package:common_shared/public.dart';
 import 'package:core_data/src/core/api_base_options.dart';
+import 'package:core_data/src/interceptors/auth_interceptor.dart';
 import 'package:core_data/src/interceptors/form_interceptor.dart';
 import 'package:core_data/src/interceptors/logger_interceptor.dart';
 import 'package:dio/dio.dart';
@@ -12,7 +13,7 @@ abstract class NetworkModule {
     return Dio()
       ..interceptors.addAll([
         const FormInterceptor(),
-        // JwtInterceptor(),
+        AuthInterceptor(),
         if (Console.isEnabled) LoggerInterceptor(),
       ])
       ..options = ApiBaseOptions();
